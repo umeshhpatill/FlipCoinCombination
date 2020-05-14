89f88144309a8b3cc30ad64a0299782ec2bfc445
umesh <usp5758@gmail.com>
2020-05-14 11:51:33 +0530

    case 5 pushed

--- FlipCoinComb.sh
+++ FlipCoinComb.sh
@@ -1,57 +1,96 @@
 #!/bin/bash -x
 
-declare -A DoubleDict
-declare -A PercentDouble
-hhh=0
-hht=0
-thh=0
-hth=0
-ttt=0
-tth=0
-htt=0
-tht=0
+declare -A CoinDict
 
-for ((count=0; count<20; count++))
+CoinDict[H]=0
+CoinDict[T]=0
+CoinDict[HT]=0
+CoinDict[TH]=0
+CoinDict[HH]=0
+CoinDict[TT]=0
+CoinDict[HHH]=0
+CoinDict[TTT]=0
+CoinDict[HTT]=0
+CoinDict[TTH]=0
+CoinDict[THT]=0
+CoinDict[HTH]=0
+CoinDict[HHT]=0
+CoinDict[TTH]=0
+
+for ((count=0; count<60; count++))
 do
-	result=$((RANDOM % 8))
-	if [ $result -eq 0 ]
+	TossChoice=$((RANDOM % 3 + 1))
+	case $TossChoice in
+	1)
+		singleToss=$((RANDOM % 2))
+		case $singleToss in
+		0)
+			$((CoinDict[H]++))
+			;;
+		1)
+			$((CoinDict[T]++))
+			;;
+		esac
+		;;
+	2)
+		coupleToss=$((RANDOM % 4))
+		case $coupleToss in
+		0)
+			$((CoinDict[HH]++))
+			;;
+		1)
+			$((CoinDict[HT]++))
+			;;
+		2)
+			$((CoinDict[TH]++))
+			;;
+		3)
+			$((CoinDict[TT]++))
+			;;
+		esac
+		;;
+	3)
+		tripleToss=$((RANDOM % 8))
+		case $tripleToss in
+		0)
+			$((CoinDict[HHH]++))
+			;;
+		1)
+			$((CoinDict[HHT]++))
+			;;
+		2)
+			$((CoinDict[THH]++))
+			;;
+		3)
+			$((CoinDict[HTH]++))
+			;;
+		4)
+			$((CoinDict[TTT]++))
+			;;
+		5)
+			$((CoinDict[TTH]++))
+			;;
+		6)
+			$((CoinDict[HTT]++))
+			;;
+		7)
+			$((CoinDict[THT]++))
+			;;
+		esac
+		;;
+	esac
+done
+
+echo ${CoinDict[@]}
+echo ${!CoinDict[@]}
+
+winCombination=`printf "%s\n" ${CoinDict[@]} | sort -nr | head -1`
+echo $winCombination
+
+for i in ${!CoinDict[@]}
+do
+	if [ ${CoinDict[$i]} -eq $winCombination ]
 	then
-		$((hhh++))
-	elif [ $result -eq 1 ]
-	then
-		$((hht++))
-	elif [ $result -eq 2 ]
-	then
-		$((thh++))
-        elif [ $result -eq 3 ]
-        then
-                $((hth++))
-        elif [ $result -eq 4 ]
-        then
-                $((ttt++))
-        elif [ $result -eq 5 ]
-        then
-                $((tth++))
-        elif [ $result -eq 6 ]
-        then
-                $((htt++))
-	elif [ $result -eq 7 ]
-	then
-		$((tht++))
+		echo "Winning Combination is "$i
 	fi
 done
-
-DoubleDict[HHH]=$hhh
-DoubleDict[TTT]=$ttt
-DoubleDict[HHT]=$hht
-DoubleDict[TTH]=$tth
-DoubleDict[THH]=$thh
-DoubleDict[HTT]=$htt
-DoubleDict[HTH]=$hth
-DoubleDict[THT]=$tht
-
-for i in ${!DoubleDict[@]}
-do
-	PercentDouble[$i]=$((${DoubleDict[$i]} * 100 / 20))
-done
-echo ${PercentDouble[@]}
