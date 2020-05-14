c4383d0b4fc790764856d0427acd7b0b868af38a
umesh <usp5758@gmail.com>
2020-05-14 11:41:04 +0530

    case 2 pushed

--- FlipCoinComb.sh
+++ FlipCoinComb.sh
@@ -1,9 +1,26 @@
 #!/bin/bash -x
 
-result=$((RANDOM % 2))
-if [ $result -eq 1 ]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+declare -A SingletDict
+
+heads=0
+tails=0
+
+for ((count=0; count<20; count++))
+do
+	result=$((RANDOM % 2))
+	if [ $result -eq 1 ]
+	then
+		$((heads++))
+	else
+		$((tails++))
+	fi
+done
+
+SingletDict[Head]=$heads
+SingletDict[Tail]=$tails
+
+for i in ${!SingletDict[@]}
+do
+	percentResult=$((${SingletDict[$i]} * 100 / 20))
+	echo $i" percentage: "$percentResult
+done
